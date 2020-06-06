8806350c8c1f4398a9da8ca31f3d37f4e532d5ac
Nathaly Rondon <nathy.madureira@gmail.com>
2020-06-06 20:27:07 -0300

    projeto finalizado

--- curso-python-ifsp-2020/projeto_final.py
+++ curso-python-ifsp-2020/projeto_final.py
@@ -26,11 +26,17 @@ def ler_notas():
 
 def calcular_media(notas):
     soma = 0
-
     for nota in notas:
        soma += nota
-    
     return soma / len(notas)
+
+def situacao_aluno(media):
+    if media >= 7:
+        return "Aprovado"
+    elif media >= 4:
+        return "Exame"
+    else:
+        return "Reprovado"
 
 def ler_aluno():
     aluno = {}
@@ -38,6 +44,7 @@ def ler_aluno():
     aluno["sexo"] = ler_sexo()
     notas_lidas = ler_notas()
     aluno["media"] = calcular_media(notas_lidas)
+    aluno["situacao"] = situacao_aluno(aluno["media"])
     return aluno 
 
 def ler_alunos():
@@ -47,11 +54,55 @@ def ler_alunos():
         alunos.append(novo_aluno)
     return alunos
 
-def print_aluno(aluno):
-    print(aluno)
+alunos = ler_alunos()
 
-print(ler_aluno())
-print(ler_alunos())
+cadastrados = len(alunos)
+aprovados = 0
+aprovadas = 0
+exame_m = 0
+exame_f = 0
+reprovados = 0
+reprovadas = 0
 
+for aluno in alunos: 
+    if aluno["sexo"] == "M":
+        if aluno["situacao"] == "Aprovado":
+            aprovados += 1
+        elif aluno["situacao"] == "Exame":
+            exame_m += 1
+        else:
+            reprovados += 1
 
+    else:
+        if aluno["situacao"] == "Aprovado":
+            aprovadas += 1
+        elif aluno["situacao"] == "Exame":
+            exame_f += 1
+        else:
+            reprovadas += 1
+ 
+if cadastrados >=1:
+    aprov = aprovadas+aprovados
+    exam = exame_f+exame_m
+    reprov = reprovadas+reprovados
 
+    total_alunos_aprovados = aprov*100/cadastrados
+    total_alunos_exame = exam*100/cadastrados
+    total_alunos_reprovados = reprov*100/cadastrados
+
+    print("." *60)
+    print("                         \033[0;1mRESULTADO:\033[m") 
+    print("." *60)
+    print(f"Alunos Cadastrados:  {cadastrados}")
+    print(f"Alunos Aprovados:    {total_alunos_aprovados:.0f}%")
+    print(f"Alunos em Exame:     {total_alunos_exame:.0f}%")
+    print(f"Alunos Reprovados:   {total_alunos_reprovados:.0f}%\n")
+    print(f"Aluna(s) Aprovada(s): {aprovadas}, Aluna(s) em Exame: {exame_f} e Aluna(s) Reprovada(s): {reprovadas}.")
+    print(f"Aluno(s) Aprovado(s): {aprovados}, Aluno(s) em Exame: {exame_m} e Aluno(s) Reprovado(s): {reprovados}.")
+    print("." *60)
+    print("                    \033[0;1mSistema Finalizado.\033[m\n")
+
+else:
+    print("." *60)
+    print("Nenhum aluno cadastrado.\n")
+    print("                    \033[0;1mSistema Finalizado.\033[m\n")
