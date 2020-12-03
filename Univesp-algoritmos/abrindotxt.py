3d09e5772e6bd2759878a63f59ab4960a82e0439
Nathaly Rondon <nathy.madureira@gmail.com>
2020-12-03 14:21:58 -0300

    master

---
+++ Univesp-algoritmos/abrindotxt.py
@@ -0,0 +1,10 @@
+def readFile(filename):
+    infile = open(filename, 'r')
+    content = infile.read()
+    infile.close()
+    wordList = content.split()
+    print(wordList)
+    return len(wordList), len(content)
+
+n_words, n_chars = readFile('texto.txt')
+print(len(wordList), len(content))
